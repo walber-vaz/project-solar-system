d1885591cd28648e75cdde9905f17ff55386997f
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-11-25 16:02:08 -0300

    10 - Renderize uma lista com as missões espaciais

--- src/components/Missions/index.jsx
+++ src/components/Missions/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import missions from '../../data/missions';
+import MissionCard from '../MissionCard';
 import Title from '../Title';
 
 export default function Missions() {
   return (
     <div data-testid="missions">
       <Title headline="Missões" />
+      {missions.map(({ name, year, country, destination }, index) => (
+        <MissionCard
+          key={ index }
+          name={ name }
+          year={ year }
+          country={ country }
+          destination={ destination }
+        />
+      ))}
     </div>
   );
 }
