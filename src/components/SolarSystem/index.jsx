b68872ed98cfa8dfbd337e36f9381ff97570bb74
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-11-25 15:39:40 -0300

    6 - Renderize uma lista com os planetas do Sistema Solar

--- src/components/SolarSystem/index.jsx
+++ src/components/SolarSystem/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import planets from '../../data/planets';
+import PlanetCard from '../PlanetCard';
 import Title from '../Title';
 
 export default function SolarSystem() {
   return (
     <div data-testid="solar-system">
       <Title headline="Planetas" />
+      {planets.map(({ name, image }) => (
+        <PlanetCard key={ name } planetName={ name } planetImage={ image } />
+      ))}
     </div>
   );
 }
